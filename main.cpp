a38fd5168ef278a465be2feb5c6198bfbb8c4546
Etienne Pasteur <etienne.pasteur@epitech.eu>
2017-10-17 01:59:05 +0800

    First commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Etienne Pasteur on 17/10/2017.
+//
+
+#include <iostream>
+
+int main(int ac, char **av) {
+
+    std::cout << "Hello world i'm back after 2 years" << std::endl;
+
+    return (0);
+}
