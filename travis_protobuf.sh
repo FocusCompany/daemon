9ac400b41b67bfd62dc8bb462e7a071d9e15a01f
Etienne Pasteur <etienne.pasteur@epitech.eu>
2017-11-19 20:35:17 +0800

    Fix travis error

--- travis_protobuf.sh
+++ travis_protobuf.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [ ! -d "$HOME/protobuf/lib" ]; then
-  wget https://github.com/google/protobuf/releases/download/v3.5.0/protobuf-cpp-3.5.0.tar.gz
-  tar -xzvf protobuf-cpp-3.5.0.tar.gz
-  cd protobuf-cpp-3.5.0 && ./configure --prefix=$HOME/protobuf && make && make install
+  wget https://github.com/google/protobuf/releases/download/v3.5.0/protobuf-cpp-3.5.0.tar.gz -O proto
+  tar -xzf proto
+  cd protobuf-3.5.0 && ./configure --prefix=$HOME/protobuf && make && make install
 else
   echo "Using cached directory."
 fi
