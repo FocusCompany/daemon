9b892cd4f12b4c8da3cb28016a7e41293f4951b2
Etienne Pasteur <etienne.pasteur@epitech.eu>
2017-11-20 00:13:36 +0800

    Fix travis error

--- travis_protobuf.sh
+++ travis_protobuf.sh
@@ -2,14 +2,16 @@
 
 set -e
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 cd $HOME/protobuf/
 
 if [ ! -d "$HOME/protobuf/lib" ]; then
   wget https://github.com/google/protobuf/archive/v3.5.0.tar.gz -O proto
   tar -xzf proto
-  cd protobuf-3.5.0 && ./autogen.sh && ./configure && make && sudo make install && sudo ldconfig
+  cd protobuf-3.5.0 && ./autogen.sh && ./configure --prefix=/usr && make && sudo make install
 else
   echo "Using cached directory."
   cd protobuf-3.5.0 && sudo make install
 fi
+
+cd $HOME/FocusCompany/daemon/Shared/Proto/
+./generate.sh
