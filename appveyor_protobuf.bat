ccc6a833fd6a660f8ca444bafd1753ddb0204954
Etienne Pasteur <etienne.pasteur@epitech.eu>
2017-11-22 17:41:56 +0800

    Try to fix Appveyor

--- appveyor_protobuf.bat
+++ appveyor_protobuf.bat
@@ -3,9 +3,9 @@ setlocal
 echo Building Protobuf Library 
 mkdir install
 dir
-cd protobuf-3.5.0
-mkdir build_protobuf
-cd build_protobuf
+cd protobuf-3.5.0/cmake
+mkdir build
+cd build
 mkdir solution
 cd solution
 cmake -G "Visual Studio 15 2017" -DCMAKE_INSTALL_PREFIX=../../../../install -Dprotobuf_BUILD_TESTS=OFF ../..
