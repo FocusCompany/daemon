2737f4d3db5daa99f428e2273c51db00d55b3a1e
Etienne Pasteur <etienne.pasteur@epitech.eu>
2017-11-22 17:32:04 +0800

    Try to fix Appveyor

---
+++ appveyor_protobuf.bat
@@ -0,0 +1,17 @@
+setlocal
+
+echo Building Protobuf Library 
+mkdir install
+dir
+cd protobuf-3.5.0
+mkdir build
+cd build
+mkdir solution
+cd solution
+cmake -G "Visual Studio 15 2017" -DCMAKE_INSTALL_PREFIX=../../../../install -Dprotobuf_BUILD_TESTS=OFF ../..
+msbuild protobuf.sln || goto error
+goto :EOF
+
+:error
+echo Failed!
+EXIT /b %ERRORLEVEL%
