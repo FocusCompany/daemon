75c1d96fd4d1785ad15220d211aee1a8c508c02f
Etienne Pasteur <etienne.pasteur@epitech.eu>
2017-11-23 00:14:37 +0800

    Add cache support to install script

--- appveyor_protobuf.bat
+++ appveyor_protobuf.bat
@@ -1,21 +1,23 @@
 setlocal
 
-echo Building Protobuf Library
-curl -L -o protobuf.zip https://github.com/google/protobuf/releases/download/v3.5.0/protobuf-all-3.5.0.zip
-7z x protobuf.zip
-del /Q protobuf.zip
 mkdir install
-cd protobuf-3.5.0/cmake
-mkdir build
-cd build
-mkdir solution
-cd solution
-cmake -G "Visual Studio 15 2017" -DCMAKE_INSTALL_PREFIX=../../../../install -Dprotobuf_BUILD_TESTS=OFF ../..
-msbuild protobuf.sln || goto error
+IF NOT exist "protobuf-3.5.0" (
+   echo Building Protobuf Library
+   curl -L -o protobuf.zip https://github.com/google/protobuf/releases/download/v3.5.0/protobuf-all-3.5.0.zip
+   7z x protobuf.zip
+   del /Q protobuf.zip
+   cd protobuf-3.5.0/cmake
+   mkdir build
+   cd build
+   mkdir solution
+   cd solution
+   cmake -G "Visual Studio 15 2017" -DCMAKE_INSTALL_PREFIX=../../../../install -Dprotobuf_BUILD_TESTS=OFF ../..
+   msbuild protobuf.sln || goto error
+) ELSE (
+  echo Using cached protobuf
+)
 echo Installing Protobuf Library 
 msbuild INSTALL.vcxproj || goto error
-cd ../../../../
-dir
 echo Moving Protobuf Library to C:\... 
 move "C:\projects\daemon\install" "C:\Program Files\protobuf"
 echo Protobuf successfully installed
