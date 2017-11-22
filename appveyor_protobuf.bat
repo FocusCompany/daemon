938806796cf087843d634602d4667f7ae26fa740
Etienne Pasteur <etienne.pasteur@epitech.eu>
2017-11-22 21:41:13 +0800

    Try to fix appveyor

--- appveyor_protobuf.bat
+++ appveyor_protobuf.bat
@@ -9,7 +9,11 @@ mkdir solution
 cd solution
 cmake -G "Visual Studio 15 2017" -DCMAKE_INSTALL_PREFIX=../../../../install -Dprotobuf_BUILD_TESTS=OFF ../..
 msbuild protobuf.sln || goto error
+echo Installing Protobuf Library 
 msbuild INSTALL.vcxproj || goto error
+cd ../../../../
+echo Moving Protobuf Library to C:\... 
+move install C:\Program Files\protobuf\
 goto :EOF
 
 :error
