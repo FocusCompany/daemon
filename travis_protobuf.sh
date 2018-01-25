4ec93b155616e9366adfacd7038ec6c4d1aa227e
Etienne Pasteur <etienne.pasteur@epitech.eu>
2018-01-25 14:22:00 +0100

    Fix travis protobuf cache

--- travis_protobuf.sh
+++ travis_protobuf.sh
@@ -3,19 +3,19 @@
 set -e
 cd $HOME/protobuf/
 
-if [ ! -d "$HOME/protobuf/lib" ]; then
+if [ ! -d "$HOME/protobuf/protobuf-3.5.1" ]; then
   echo "Building Protobuf Library"
-  wget https://github.com/google/protobuf/releases/download/v3.5.0/protobuf-all-3.5.0.tar.gz -O protobuf.tar.gz
+  wget https://github.com/google/protobuf/releases/download/v3.5.1/protobuf-all-3.5.1.tar.gz -O protobuf.tar.gz
   tar -xzf protobuf.tar.gz
   rm protobuf.tar.gz
   if [[ $1 = "osx" ]]; then
-    cd protobuf-3.5.0 && ./autogen.sh && ./configure && make
+    cd protobuf-3.5.1 && ./autogen.sh && ./configure && make
   else
-    cd protobuf-3.5.0 && ./autogen.sh && ./configure --prefix=/usr && make
+    cd protobuf-3.5.1 && ./autogen.sh && ./configure --prefix=/usr && make
   fi
 else
   echo "Using cached protobuf"
-  cd protobuf-3.5.0
+  cd protobuf-3.5.1
 fi
 echo "Installing Protobuf Library to /usr or /usr/local/ (Linux/Mac)"
 sudo make install
