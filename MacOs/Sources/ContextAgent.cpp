3a3e3bc644afe29f4a4c37ad3d9bbda629d9e6d1
Etienne Pasteur <etienne.pasteur@epitech.eu>
2018-04-20 16:14:07 +0800

    Add Macos windows listener

--- MacOs/Sources/ContextAgent.cpp
+++ MacOs/Sources/ContextAgent.cpp
@@ -8,7 +8,8 @@
 #include <FocusSerializer.hpp>
 #include "ContextAgent.hpp"
 #include "FocusContextEventPayload.pb.h"
-#include <Carbon/Carbon.h>
+#include <spdlog/spdlog.h>
+#include <array>
 
 ContextAgent::ContextAgent() {
 
@@ -19,31 +20,30 @@ ContextAgent::~ContextAgent() {
 }
 
 void ContextAgent::Run() {
-    //TODO: Implementing ContextAgent
-
-    int layer;
-    std::string tmp;
-
-    CFArrayRef windowList = CGWindowListCopyWindowInfo(kCGWindowListOptionOnScreenOnly, kCGNullWindowID);
-    CFIndex numWindows = CFArrayGetCount(windowList);
-    for (int i = 0; i < (int) numWindows; i++) {
-        auto info = static_cast<CFDictionaryRef>(CFArrayGetValueAtIndex(windowList, i));
-        auto appName = static_cast<CFStringRef>(CFDictionaryGetValue(info, kCGWindowOwnerName));
-        auto windowName = static_cast<CFStringRef>(CFDictionaryGetValue(info, kCGWindowName));
-        CFNumberGetValue(static_cast<CFNumberRef>(CFDictionaryGetValue(info, kCGWindowLayer)), kCFNumberIntType, &layer);
-        if (appName != nullptr && layer == 0) {
-            tmp = CFStringGetCStringPtr(appName, kCFStringEncodingMacRoman);
-            std::cout << "Layer: " << layer << " AppName: " << tmp;
-            if (windowName != nullptr) {
-                tmp = CFStringGetCStringPtr(windowName, kCFStringEncodingMacRoman);
-                std::cout << " WindowName: " << tmp;
+    std::string cmd = "osascript ./MacOs/printAppTitle.scpt";
+    std::string oldProcessName;
+    std::string oldWindowsTitle;
+    while (true) {
+        std::array<char, 256> buffer{};
+        std::string result;
+        std::shared_ptr<FILE> pipe(popen(cmd.c_str(), "r"), pclose);
+        if (!pipe) {
+            spdlog::get("logger")->error("Failed to retrieve current AppName");
+        } else {
+            while (!feof(pipe.get())) {
+                if (fgets(buffer.data(), 256, pipe.get()) != nullptr)
+                    result += buffer.data();
             }
-            std::cout << std::endl;
+            std::string processName = result.substr(1, result.find("\",\"") - 1);
+            std::string windowsTitle = result.substr(result.find("\",\"") + 3, result.size() - (result.find("\",\"") + 3) - 2);
+            if (oldProcessName != processName || oldWindowsTitle != windowsTitle) {
+                oldProcessName = processName;
+                oldWindowsTitle = windowsTitle;
+                OnContextChanged(processName, windowsTitle);
+            }
         }
+        sleep(2);
     }
-    CFRelease(windowList);
-
-    std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::hours((std::numeric_limits<int>::max)()));
 }
 
 void ContextAgent::OnContextChanged(const std::string &processName, const std::string &windowTitle) const {
